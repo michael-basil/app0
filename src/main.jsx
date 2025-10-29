df08e5e085c465b4b10a25049944d4341fd390f2
Michael Basil <michael@basil.one>
2025-10-29 16:37:40 -0500

    feat: auth0 google social login button and wiring

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const domain = import.meta.env.VITE_AUTH0_DOMAIN
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
+const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{ redirect_uri: redirectUri }}
+    useRefreshTokens
+    cacheLocation="localstorage"
+  >
     <App />
-  </StrictMode>,
+  </Auth0Provider>
 )
